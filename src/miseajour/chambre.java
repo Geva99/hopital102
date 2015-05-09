54246280c2d7239c043561bf3b9556fb2e6c6e22
Twentycent_ONE <Twentycent_ONE@TwentycentONE->
2015-05-09 17:29:43 +0200

    -Vincent

--- src/miseajour/chambre.java
+++ src/miseajour/chambre.java
@@ -21,7 +21,7 @@ class chambre extends miseajour{
     chambre chambr = new chambre(); 
     public void insertchambre (String codeSer, String numCha, String surveillant, String nbLits) throws SQLException
     {
-        String cmd = "INSERT INTO chambre VALUES ('codeSer','numCha','surveillant','nbLits')";
+        String cmd = "INSERT INTO chambre VALUES ('"+codeSer+"','"+numCha+"','"+surveillant+"','"+nbLits+"')";
         chambr.executeUpdate(cmd);
     }
     
@@ -30,20 +30,20 @@ class chambre extends miseajour{
     
     public void modifChambre (String codeSer, String numCha, String numSurv, String nbLits) throws SQLException
     {
-        String maj1 = "UPDATE chambre SET surveillant = 'numSurv' WHERE (code_service = 'codeSer' && no_chambre = 'numCha')";
+        String maj1 = "UPDATE chambre SET surveillant = '"+numSurv+"' WHERE (code_service = '"+codeSer+"' && no_chambre = '"+numCha+"')";
         chambr.executeUpdate (maj1);
-        String maj2 = "UPDATE chambre SET code_service = 'codeSer' WHERE (code_service = 'codeSer' && no_chambre = 'numCha')";
+        String maj2 = "UPDATE chambre SET code_service = '"+codeSer+"' WHERE (code_service = '"+codeSer+"' && no_chambre = '"+numCha+"')";
         chambr.executeUpdate (maj2);
-        String maj3 = "UPDATE chambre SET no_chambre = 'numCha' WHERE (code_service = 'codeSer' && no_chambre = 'numCha')";
+        String maj3 = "UPDATE chambre SET no_chambre = '"+numCha+"' WHERE (code_service = '"+codeSer+"' && no_chambre = '"+numCha+"')";
         chambr.executeUpdate (maj3);
-        String maj4 = "UPDATE chambre SET nb_lits = 'nbLits' WHERE (code_service = 'codeSer' && no_chambre = 'numCha')";
+        String maj4 = "UPDATE chambre SET nb_lits = '"+nbLits+"' WHERE (code_service = '"+codeSer+"' && no_chambre = '"+numCha+"')";
         chambr.executeUpdate (maj4);
         
     }
     
     public void supprChambre (String codeSer, String numCha) throws SQLException
     {
-        String cmd = "DELETE FROM chambre WHERE (code_service = 'codeSer' && no_chambre = 'numCha')";
+        String cmd = "DELETE FROM chambre WHERE (code_service = '"+codeSer+"' && no_chambre = 'numCha')";
         chambr.executeUpdate(cmd);
     }
     
--- src/miseajour/hospitalisation.java
+++ src/miseajour/hospitalisation.java
@@ -16,24 +16,28 @@ import Stockage.Hospitalisation;
  * @author Twentycent_ONE
  */
 public class hospitalisation extends miseajour {
-        public void inserthospitalisation (String codeSer, String numCha, String surveillant, String nbLits)
+        public void inserthospitalisation (String codeSer, String numCha, String surveillant, int lit) throws SQLException
     {
         String cmd = "INSERT INTO hospitalisation VALUES ('codeSer','numCha','surveillant','nbLits')";
-        ajouterRequete(cmd);
+        executeUpdate(cmd);
     }
-    public void modifhospitalisationk (String numSurv, String codeSer, String numCha)
+    public void modifhospitalisation (String numMalade, String codeSer, String numCha, int lit) throws SQLException
     {
-        ajouterRequete("UPDATE hospitalisation SET surveillant = 'numSurv' WHERE (code_service = 'codeSer' && no_chambre = 'numCha')");
-    }
-    public void modifhospitalisation (String numSurv, String codeSer, String numCha)
-    {
-        ajouterRequete("UPDATE hospitalisation SET surveillant = 'numSurv' WHERE (code_service = 'codeSer' && no_chambre = 'numCha')");
-    }
-    
+        String maj1 = "UPDATE hospitalisation SET no_malade = '"+numMalade+"' WHERE (no_malade = '"+numMalade+"')";
+        executeUpdate(maj1);
+        String maj2 = "UPDATE hospitalisation SET code_service = '"+codeSer+"' WHERE (no_malade = '"+numMalade+"')";
+        executeUpdate(maj2);
+        String maj3 = "UPDATE hospitalisation SET no_chambre = '"+numCha+"' WHERE (no_malade = '"+numMalade+"')";
+        executeUpdate(maj3);
+        String maj4 = "UPDATE hospitalisation SET no_chambre = '"+lit+"' WHERE (no_malade = '"+numMalade+"')";
+        executeUpdate(maj4);
+        
+    }   
     
     public void supprhospitalisation (String numSurv, String codeSer, String numCha)
     {
-        ajouterRequete("DELETE FROM hospitalisation WHERE (code_service = 'codeSer' && no_chambre = 'numCha')");
+        String cmd = "DELETE FROM hospitalisation WHERE (code_service = '"+codeSer+"' && no_chambre = '"+numCha+"')"
+        executeUpdate(cmd);
     }
     
 }
