6b18f953e0b29514904925ebdeea396112ec3a88
Erika <Erika@Kika>
2015-05-10 17:51:15 +0200

    Erika

--- src/miseajour/docteur.java
+++ src/miseajour/docteur.java
@@ -27,12 +27,15 @@ public class docteur extends miseajour{
 
     public void modifDoc (int numerodoc, String spedoc) throws SQLException
     {
-        //On modifie un employé
-        String maj1 = "UPDATE docteur SET employe.numero == '"+numerodoc+"' WHERE (employe.numero=='"+numerodoc+"')";
+        //On modifie un docteur
+        String maj1 = "UPDATE docteur SET docteur.numero == '"+numerodoc+"' WHERE (docteur.numero=='"+numerodoc+"')";
         doc.executeUpdate (maj1);
-        String maj2 = "UPDATE docteur SET employe.nom == '"+spedoc+"' WHERE (employe.numero=='"+numerodoc+"')";
+        String maj2 = "UPDATE docteur SET docteur.specialité == '"+spedoc+"' WHERE (docteur.numero=='"+numerodoc+"')";
         doc.executeUpdate (maj2);
         
+            //On modifie en même temps un employé
+        String maj3 = "UPDATE employé SET employé. == '"+numerodoc+"' WHERE (employe.numero=='"+numerodoc+"')";
+        doc.executeUpdate (maj3);
         // On modifie ou on essaie de modifier dans les tables infirmier et docteur:
         
     }
@@ -41,7 +44,8 @@ public class docteur extends miseajour{
     {
         String cmd1 = "DELETE FROM employe WHERE (employe.numero=='"+numerodoc+"')";
         doc.executeUpdate(cmd1);
-//        String cmd2 = 
+        String cmd2 = "DELETE FROM docteur WHERE (docteur.numero=='"+numerodoc+"')";
+        doc.executeUpdate(cmd1);
     }
     
 }
